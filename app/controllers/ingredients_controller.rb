03faffc40e8ed9fe631016aa447fd5373be7e833
Tori <victoria.ashley.wood@gmail.com>
2016-03-08 10:53:34 -0500

    updated ingredients controller and views with edit, update, create, new, and destroy actions

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -6,18 +6,42 @@ class IngredientsController < ApplicationController
   end
 
   def new
+    @ingredient = Ingredient.new
+    @recipe = Recipe.find(params[:ingredient][:recipe_id])
   end
 
   def edit
+    @recipe = Recipe.find(ingredient.recipe_id)
   end
 
   def update
+    ingredient.update(ingredient_params)
+
+    if @ingredient.save
+      flash[:success] = "Updated successfully"
+      redirect_to recipe_path(ingredient_params[:recipe_id])
+    else
+      flash[:danger] = "There was a problem updating this record."
+      redirect_to edit_ingredient_path(params[:recipe_id])
+    end
   end
 
   def create
+    @ingredient = Ingredient.create(ingredient_params)
+
+    if @ingredient.save
+      flash[:success] = "Added successfully"
+      redirect_to recipe_path(ingredient_params[:recipe_id])
+    else
+      flash[:danger] = "There was a problem adding this record."
+      redirect_to new_ingredient_path(params[:recipe_id])
+    end
   end
 
   def destroy
+    @recipe = Recipe.find(ingredient.recipe_id)
+    ingredient.destroy
+    redirect_to recipe_path(@recipe)
   end
 
   private
@@ -25,6 +49,8 @@ class IngredientsController < ApplicationController
   def ingredient
     @ingredient ||= Ingredient.find(params[:id])    
   end
+
+  helper_method :ingredient
 
   def ingredient_params
     params.require(:ingredient).permit(:name, :quantity, :recipe_id)
