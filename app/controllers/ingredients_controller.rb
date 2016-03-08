65d67d32a521949c432679079fe9df9ddd88f327
Tori <victoria.ashley.wood@gmail.com>
2016-03-08 08:43:27 -0500

    added controller and views for ingredients

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,21 @@
+class IngredientsController < ApplicationController
+    
+  def index
+    
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
